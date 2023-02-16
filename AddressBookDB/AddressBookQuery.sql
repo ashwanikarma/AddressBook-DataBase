ac37d2573a5d177ecde0f25edd4bbd63fd84d9ff
ashwanikarma <ashwanikarma9@gmail.com>
2023-02-17 01:07:07 +0530

    [Ashwani] - created table

--- AddressBookDB/AddressBookQuery.sql
+++ AddressBookDB/AddressBookQuery.sql
@@ -4,3 +4,14 @@
 create database AddressBookDatabase;
 use AddressBookDatabase;
 
+----UC 2: Create Table----
+create table AddressBookDetails(
+firstName varchar(200) not null,
+lastName varchar(200) not null,
+address varchar(255) not null,
+city varchar(255),
+state varchar(255),
+zip int,
+phoneNumber bigint,
+email varchar(200) not null
+)
