f72a7cec3a44c6484812ce5ac8185f451d13d9a9
ashwanikarma <ashwanikarma9@gmail.com>
2023-02-17 01:12:26 +0530

    [Ashwani] - inserted to table

--- AddressBookDB/AddressBookQuery.sql
+++ AddressBookDB/AddressBookQuery.sql
@@ -15,3 +15,16 @@ zip int,
 phoneNumber bigint,
 email varchar(200) not null
 )
+
+------ UC 3: Insert Values to Table ------
+insert into  AddressBookDetails
+values('Ashwani', 'Karma', '54 backstreet', 'NY', 'America', 123654, 09131715292, 'ashwani@email.com'),
+('geetika', 'nag', '12 SM nagar', 'banglore', 'Karnataka', 100004, 70223865421, 'geetika@email.com'),
+('gungun', 'karrma', '3 AB nagar', 'Noida', 'UP', 543201, 07869296597, 'karma@email.com'),
+('khushi', 'nag', '84 MJ nagar', 'Chennai', 'Tamil Nadu', 129123, 09425594992, 'khushi@email.com')
+
+--display table
+select * from AddressBookDetails
+
+
+
